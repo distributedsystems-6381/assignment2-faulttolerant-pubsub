23e8b0f3fc692264f8e118ea58eb783b2713fbb5
Satish Ranjan <satishranjan.github@gmail.com>
2021-02-19 20:30:02 -0800

    adding lamebroker

--- publisher_app.py
+++ publisher_app.py
@@ -1,8 +1,10 @@
 import sys
 import time
+import zmq
 from random import randrange
 import direct_pub_middleware as dmw
 import broker_pub_middleware as bmw
+import host_ip_provider
 
 
 # METHODS
@@ -48,17 +50,59 @@ def broker_messaging_strategy(ips_ports, topics):
 # create base topics & extract strategy
 publish_topics = ["temp", "humidity"]
 strategy = sys.argv[1] if len(sys.argv) > 1 else print("Please submit valid strategy (direct || broker)")
+if strategy != 'direct' and strategy != 'broker':
+    print("Please submit valid strategy (direct || broker)")
+    sys.exit()
 
-# add additional topics if provided
-if len(sys.argv) > 3:
-    for arg in sys.argv[3:]:
-        publish_topics.append(arg)
+#python3 publisher.py direct "10.0.0.6:4000" 5000
+publisher_port = ""
+if strategy == "direct":
+    broker_ip_port = ""
+    if len(sys.argv) > 2:
+        broker_ip_port = sys.argv[2]         
 
-    print("topics to publish: {}".format(publish_topics))
+    if broker_ip_port == "":
+        print("No broker ip:port provided")
+        sys.exit()
+    
+    #Add additional topics if provided for the direct strategy
+    if len(sys.argv) > 3:
+        publisher_port = sys.argv[3]
+
+    if len(sys.argv) > 4:
+        for arg in sys.argv[4:]:
+            publish_topics.append(arg)
+
+    #Register the publisher to the broker
+    context = zmq.Context()
+    print("Connecting to broker at ip:port=> {}".format(broker_ip_port))
+    broker_socket = context.socket(zmq.REQ)
+    broker_socket.connect("tcp://{}".format(broker_ip_port))
+
+    publisher_ip_port = host_ip_provider.get_host_ip() + ":" + publisher_port
+
+    register_publisher_data_to_broker = publisher_ip_port + '#'
+    counter = 1
+    for topic in publish_topics:
+        if counter < len(publish_topics):
+            register_publisher_data_to_broker = register_publisher_data_to_broker + topic + ','
+        else:
+            register_publisher_data_to_broker = register_publisher_data_to_broker + ',' + topic
+
+    print("Registering publisher to the broker: {}".format(register_publisher_data_to_broker))
+    broker_socket.send_string(register_publisher_data_to_broker)
+    message = broker_socket.recv_string()
+    print("Message received from broker: {}".format(message))
+else:
+    #Add additional topics if provided for the broker strategy
+    if len(sys.argv) > 3:
+        for arg in sys.argv[3:]:
+            publish_topics.append(arg)
+
+print("Topics to publish: {}".format(publish_topics))
 
 # initiate messaging based on which strategy is submitted
-if strategy == "direct":
-    publisher_port = sys.argv[2] if len(sys.argv) > 2 else "5555"
+if strategy == "direct":   
     direct_messaging_strategy(publisher_port, publish_topics)
 elif strategy == "broker":
     broker_ip_port = sys.argv[2] if len(sys.argv) > 2 else "10.0.0.2:5559"
